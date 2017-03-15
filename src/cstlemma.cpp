417a8b463859ecd409227b8ccac1a8550a8e6d43
Bart Jongejan <bart@cst.dk>
2017-03-15 11:08:27 +0100

    New version

--- src/cstlemma.cpp
+++ src/cstlemma.cpp
@@ -19,8 +19,8 @@ You should have received a copy of the GNU General Public License
 along with CSTLEMMA; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-#define CSTLEMMAVERSION "7.15"
-#define CSTLEMMADATE "2016.10.19"
+#define CSTLEMMAVERSION "7.16"
+#define CSTLEMMADATE "2017.03.15"
 #define CSTLEMMACOPYRIGHT "2002-2016 Center for Sprogteknologi"
 
 #include "lemmatiser.h"
