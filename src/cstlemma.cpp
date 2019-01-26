3ed15bf0874af1f6320d43bf273ce5c776fc1c93
Bart Jongejan <bart@cst.dk>
2019-01-27 00:56:55 +0100

    New version that solves a (rare) problem: lemma prefixed by word

--- src/cstlemma.cpp
+++ src/cstlemma.cpp
@@ -19,8 +19,8 @@ You should have received a copy of the GNU General Public License
 along with CSTLEMMA; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-#define CSTLEMMAVERSION "7.36"
-#define CSTLEMMADATE "2018.9.2"
+#define CSTLEMMAVERSION "7.37"
+#define CSTLEMMADATE "2019.1.27"
 #define CSTLEMMACOPYRIGHT "2002-2018 Center for Sprogteknologi"
 
 #include "lemmatiser.h"
