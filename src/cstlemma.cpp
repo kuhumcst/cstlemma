f4f4865311fced50be868613b8c2e320333d43e8
Bart Jongejan <zgk261@ku.dk>
2026-06-30 08:50:06 +0200

    Adjusted version info

--- src/cstlemma.cpp
+++ src/cstlemma.cpp
@@ -19,9 +19,9 @@ You should have received a copy of the GNU General Public License
 along with CSTLEMMA; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-#define CSTLEMMAVERSION "8.5.4"
-#define CSTLEMMADATE "2025.06.26"
-#define CSTLEMMACOPYRIGHT "2002-2025 Center for Sprogteknologi"
+#define CSTLEMMAVERSION "8.5.5"
+#define CSTLEMMADATE "2026.06.30"
+#define CSTLEMMACOPYRIGHT "2002-2026 Center for Sprogteknologi"
 
 #include "lemmatiser.h"
 #include "option.h"
